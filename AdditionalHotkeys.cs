a5f6be536db95d731725b7ef52e64a33df2e96a4
Andrew Rumak <deadcows@yandex.ru>
2014-07-01 09:55:36 +0400

    Update AdditionalHotkeys.cs
    formatting

--- AdditionalHotkeys.cs
+++ AdditionalHotkeys.cs
@@ -11,27 +11,27 @@ public class AdditionalHotkeys
 
 	[MenuItem("Window/Toggle Lock &q")]
 	static void ToggleInspectorLock()
-    {
-        if (_mouseOverWindow == null)
-        {
-            if (!EditorPrefs.HasKey("LockableInspectorIndex"))
-                EditorPrefs.SetInt("LockableInspectorIndex", 0);
-            int i = EditorPrefs.GetInt("LockableInspectorIndex");
- 
-            Type type = Assembly.GetAssembly(typeof(Editor)).GetType("UnityEditor.InspectorWindow");
-            Object[] findObjectsOfTypeAll = Resources.FindObjectsOfTypeAll(type);
-            _mouseOverWindow = (EditorWindow)findObjectsOfTypeAll[i];
-        }
- 
-        if (_mouseOverWindow != null && _mouseOverWindow.GetType().Name == "InspectorWindow")
-        {
-            Type type = Assembly.GetAssembly(typeof(Editor)).GetType("UnityEditor.InspectorWindow");
-            PropertyInfo propertyInfo = type.GetProperty("isLocked");
-            bool value = (bool)propertyInfo.GetValue(_mouseOverWindow, null);
-            propertyInfo.SetValue(_mouseOverWindow, !value, null);
-            _mouseOverWindow.Repaint();
-        }
-    }
+	{
+		if (_mouseOverWindow == null)
+		{
+			if (!EditorPrefs.HasKey("LockableInspectorIndex"))
+				EditorPrefs.SetInt("LockableInspectorIndex", 0);
+			int i = EditorPrefs.GetInt("LockableInspectorIndex");
+
+			Type type = Assembly.GetAssembly(typeof(Editor)).GetType("UnityEditor.InspectorWindow");
+			Object[] findObjectsOfTypeAll = Resources.FindObjectsOfTypeAll(type);
+			_mouseOverWindow = (EditorWindow)findObjectsOfTypeAll[i];
+		}
+
+		if (_mouseOverWindow != null && _mouseOverWindow.GetType().Name == "InspectorWindow")
+		{
+			Type type = Assembly.GetAssembly(typeof(Editor)).GetType("UnityEditor.InspectorWindow");
+			PropertyInfo propertyInfo = type.GetProperty("isLocked");
+			bool value = (bool)propertyInfo.GetValue(_mouseOverWindow, null);
+			propertyInfo.SetValue(_mouseOverWindow, !value, null);
+			_mouseOverWindow.Repaint();
+		}
+	}
 
 	[MenuItem("Window/Clear Console #&c")]
 	static void ClearConsole()
